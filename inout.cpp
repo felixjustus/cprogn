85d7f318fe0624fe758c9e9f77c4d49b088fbe83
Felix Justus <felixjustus95@gmail.com>
2019-04-26 22:13:18 +0200

    case mit while getausht versuch

--- inout.cpp
+++ inout.cpp
@@ -7,10 +7,17 @@ int main(void){
   cout << "Willst du nochmal? (y/n)" << endl;
   cin >> antwort;
   
-  switch(antwort){
+  //TODO if != Y/N
+
+
+
+
+  //? switch mit ner char abfrage geht irgendwie nicht...
+/*  switch(antwort){
   case 0: antwort = 'y'; cout << "NOCHMAAL\n"; break;
   case 1: antwort = 'n'; cout << "Echt nicht?\n"; break;
   default : cout << "Bitte benutz nur y/n KLEINGESCHRIEBEN!\n";
   }
+*/
   
 }
