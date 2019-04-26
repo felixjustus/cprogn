d26f469a1349bacbb9f053e9a35129886a2712aa
felixjustus <felixjustus95@gmail.com>
2019-04-26 11:48:03 +0200

    new files from mac

---
+++ helloworld.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std; 
+
+//TODO using namespace std macht dass std::cout zu -> cout 
+/*
+!Außerdem!
+!kann man sowas wie 
+!   cout << "bla"
+!        << "bla"
+!        << "bla";
+statt
+!
+!   std::cout << "blaaaa";
+!   std::cout << "blaaaa";
+!   std::cout << "blaaaa";
+!   std::cout << "blaaaa";
+*/
+
+int main(void){
+  int i;
+ 
+  for(i=0; i<2; i++){
+    std::cout << "Hello World!\t";
+  }
+
+  cin.sync();
+  cin.get();
+  return 0;
+}
---
+++ inout.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main(void){
+  char y,n,antwort;
+  cout << "Willst du nochmal? (y/n)" << endl;
+  cin >> antwort;
+  
+  switch(antwort){
+  case 0: antwort = y; cout << "NOCHMAAL\n"; break;
+  case 1: antwort = n; cout << "Echt nicht?\n"; break;
+  default : cout << "Bitte benutz nur y/n KLEINGESCHRIEBEN!\n";
+  }
+  
+}
