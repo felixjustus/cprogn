14828722e62ae254ea097eeb66833868bb2524c5
Felix Justus <felixjustus95@gmail.com>
2019-04-29 20:13:36 +0200

    main

---
+++ aldu1selectionsort.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+//typedef int MAX = 50;
+/*
+TODO-selectionsort mit arrays realisieren
+TODO-größe anpassbar an input
+TODO-modular/klassen
+TODO-zahlen per eingabe in die console übergeben
+TODO-rekursiv
+*/
+
+int main(void){
+  int groesse;
+  int liste[groesse];
+
+  groesse = 10;
+  rand() % 100;
+}
