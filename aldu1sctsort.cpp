913c2dffcc9f45ba2402b2a838a816c3606666ee
Felix Justus <felixjustus95@gmail.com>
2019-04-30 13:27:33 +0200

    findMin() hinzugefügt, aber nicht vollständig

--- aldu1sctsort.cpp
+++ aldu1sctsort.cpp
@@ -1,23 +1,36 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 //typedef int MAX = 50;
 /*
-TODO-selectionsort mit arrays realisieren
-TODO-größe anpassbar an input
-TODO-modular/klassen
-TODO-zahlen per eingabe in die console übergeben
-TODO-rekursiv
+  TODO-selectionsort mit arrays realisieren
+  TODO-größe anpassbar an input
+  TODO-modular/klassen
+  TODO-zahlen per eingabe in die console übergeben
+  TODO-rekursiv
 */
 
-int main(void){
-  int groesse=10;
-  int liste[groesse];
 
+
+
+
+int findMin(){                    //? function(int array[], int groesse)   <- array übergeben
+  int minMarker;
   
-  //! while (liste[groesse] =! '/0'){ so hätte ich es gern gelöst , aber '/0' ist nur für chars...
+  for(int i=0; i<groesse; i++){
+    liste[i]
+  }
+}
+
+int main(void){
+  int groesse=10;                 //? int groesse als globale variable initialisieren und anschließend durch übergabe in der conole integer festlegen (in der main)
+  int liste[groesse];
+  srand(time(NULL));              //? Nicht vergessen, sonst sind die Zahlen nicht randm!
+
   for (int i=0; i<groesse; i++){
     liste[i] = rand() % 101;
-    cout << liste[i];
+    cout << liste[i] << endl;
   }
   cout << "Fertig";
   return 0;
