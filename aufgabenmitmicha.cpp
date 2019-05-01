ebe98095e8da3b2b349254d79f1f2eab97256ff0
Felix Justus <felixjustus95@gmail.com>
2019-05-01 13:25:45 +0200

    aufgabe 1 fertig

--- aufgabenmitmicha.cpp
+++ aufgabenmitmicha.cpp
@@ -1,6 +1,13 @@
 //! <16:18:02>
 #include <iostream>
+using namespace std;
 
+//?Aufgabe 1
 int main(void){
-  cout <<
+  cout << "\n \n ----->WOOOW<----- \nDieses ist ein Scheiss-Programm!\n";
+  cout << "Und es ist ganz allein(noch)." << " -> Schau mal Micha! Noch ein umbruch! (mit endl;)" << endl;
+  cout << "---->Magikk!!<----" << endl;
+
+  
 }
+
