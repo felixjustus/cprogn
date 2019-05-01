ef1589871a1d600687c72b8958dcd079058312bf
Felix Justus <felixjustus95@gmail.com>
2019-05-01 15:57:06 +0200

    aufgabe mit micha

--- aufgabenmitmicha.cpp
+++ aufgabenmitmicha.cpp
@@ -1,13 +1,25 @@
 //! <16:18:02>
 #include <iostream>
+#include <string>
+#include <string>
 using namespace std;
 
-//?Aufgabe 1
+//? Aufgabe 1
 int main(void){
   cout << "\n \n ----->WOOOW<----- \nDieses ist ein Scheiss-Programm!\n";
   cout << "Und es ist ganz allein(noch)." << " -> Schau mal Micha! Noch ein umbruch! (mit endl;)" << endl;
-  cout << "---->Magikk!!<----" << endl;
+  cout << "---->Magikk!!<----\n\n\n" << endl;
 
-  
+//? Augabe 2
+  cout << "Jetzt werden deine Angaben überprüft. Bitte einzeln angeben!\nZuerst deinen Namen bitte:" << endl;
+
+std::string line;
+getline(std::cin, line);  // get the entire line
+
+// parse each string from the line
+std::istringstream stream(line);
+for (int i=0; stream.good(); i++) {
+  stream >> input[i];
+}
 }
 
