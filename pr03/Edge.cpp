550e1d2a2cb23f1f93e96a7fbc516de0a000e842
Felix Justus <felixjustus95@gmail.com>
2019-05-13 13:30:23 +0200

    Node/Edge erstellt und befüllt

---
+++ pr03/Edge.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <utility>
+using namespace std;
+
+float getWeight(void)
+Node * getStartNode(void)
+Node * getEndNode(void)
+void setWeight(float w)
+void setStartNode(Node * n)
+void setEndNode(Node * n)
--- pr03/node.cpp
+++ pr03/node.cpp
@@ -1,1 +1,25 @@
-//hier kommt die node rein :D
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <utility>
+using namespace std;
+
+std::string getKey(void){
+  
+}
+
+std<Edge*> getEdges(void){
+
+}
+
+std::string setKey(void){
+
+}
+
+void setNewEdge(Edge * edge){
+
+}
+
+
+//std::string node_key
+//Liste<Edge*> edges
